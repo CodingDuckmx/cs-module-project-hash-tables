ecc0b5852a8ff510cffda07667b6aadb37ab2cbc
Coding Duck <57272996+CodingDuckmx@users.noreply.github.com>
2020-09-06 23:15:12 -0500

    Almost everything OK, large test on summdiff crashes.

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,50 @@
 
 # Your code here
 
+with open('ciphertext.txt') as f:
+    script = f.read()
+
+dct = {}
+
+script = script.replace('\n',' ')
+
+
+for i in script:
+
+    if (i.isalpha() == False) or (i.isupper() == False):
+
+        continue
+
+    if i not in dct:
+
+        dct[i] = 0
+    
+    dct[i] += 1
+
+def getKey(item):
+
+  return item[1]
+
+alphabet = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+final_dct = {letter:None for letter in alphabet}
+
+sorted_dict = sorted(dct.items(),key=getKey)
+
+for i in range(1,27):
+
+   final_dct[sorted_dict[i-1][0]] = alphabet[-i]
+
+new_script = ''
+
+for i in script:
+
+    if i in final_dct:
+
+        new_script += final_dct[i]
+    
+    else:
+
+        new_script += i
+
+print(new_script)
--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,9 +3,11 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
+import itertools
+
 #q = set(range(1, 10))
-#q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
+q = set(range(1, 200))
+# q = (1, 3, 4, 7, 12)
 
 
 def f(x):
@@ -13,3 +15,10 @@ def f(x):
 
 # Your code here
 
+Q = {(a,b,c,d): a+b-c+d+3 for a,b,d in itertools.product(q,repeat = 3) for c in q}
+for pair, result in Q.items():
+
+  if result == 0:
+
+    print(f'f({pair[0]}) + f({pair[1]}) = f({pair[2]}) - f({pair[3]})'
+       f'    {f(pair[0])} + {f(pair[1])} = {f(pair[2])} - {f(pair[3])}')
